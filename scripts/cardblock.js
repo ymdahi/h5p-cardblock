2224cd01aa4e5ddb0a2a4b0e42cd1aaf8f7c0f1a
ydahi <ymdahi@gmail.com>
2019-11-05 15:03:21 -0500

    added styles; new constructor

--- scripts/cardblock.js
+++ scripts/cardblock.js
@@ -1,19 +1,36 @@
 var H5P = H5P || {};
-
-/**
- * Constructor.
- *
- * @param {object} params Options for this library.
- */
-H5P.CardBlock = function (params) {
-  this.cardText = params.text === undefined ? '<em>New text</em>' : params.text;
-};
-
-/**
- * Wipe out the content of the wrapper and put our HTML in it.
- *
- * @param {jQuery} $wrapper
- */
-H5P.CardBlock.prototype.attach = function ($wrapper) {
-  $wrapper.addClass('h5p-text').html(this.cardText);
-};
+ 
+H5P.CardBlock = (function ($) {
+  /**
+   * Constructor function.
+   */
+  function C(options, id) {
+    // Extend defaults with provided options
+    this.options = $.extend(true, {}, {
+      cardTitle: 'Hello world!',
+      cardImage: null
+    }, options);
+    // Keep provided id.
+    this.id = id;
+  };
+ 
+  /**
+   * Attach function called by H5P framework to insert H5P content into
+   * page
+   *
+   * @param {jQuery} $container
+   */
+  C.prototype.attach = function ($container) {
+    // Set class on container to identify it as a greeting card
+    // container.  Allows for styling later.
+    $container.addClass("h5p-cardblock");
+    // Add image if provided.
+    if (this.options.cardImage && this.options.cardImage.path) {
+      $container.append('<img class="card-image" src="' + H5P.getPath(this.options.cardImage.path, this.id) + '">');
+    }
+    // Add greeting text.
+    $container.append('<div class="card-title">' + this.options.cardTitle + '</div>');
+  };
+ 
+  return C;
+})(H5P.jQuery);
