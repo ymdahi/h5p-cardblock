fcdbee65070541bedc1c1540799c4465f520cc22
ydahi <ymdahi@gmail.com>
2019-11-06 10:01:38 -0500

    wrap html render in for loop

--- scripts/cardblock.js
+++ scripts/cardblock.js
@@ -16,58 +16,58 @@ H5P.CardBlock = (function ($) {
 
   };
 
-    
+  
+  for (var i = 0; i < this.options.cards.length; i++) {
 
+    /**
+     * Attach function called by H5P framework to insert H5P content into
+     * page
+     *
+     * @param {jQuery} $container
+     */
+    C.prototype.attach = function ($container) {
+      $container.addClass("h5p-cardblock-container");
+      $container.append('<div class="cardblock"></div>');
+      var cardTarget = $('.cardblock');
 
-
-  /**
-   * Attach function called by H5P framework to insert H5P content into
-   * page
-   *
-   * @param {jQuery} $container
-   */
-  C.prototype.attach = function ($container) {
-    $container.addClass("h5p-cardblock-container");
-    $container.append('<div class="cardblock"></div>');
-    var cardTarget = $('.cardblock');
-
-    // Add image if provided.
-    if (this.options.cardImage && this.options.cardImage.path) {
-      var cardMedia = `<div class="card-media"><img class="card-image" src="${H5P.getPath(this.options.cardImage.path, this.id)}"></div>`
-      cardTarget.append(cardMedia);
-      //$cardTarget.append('<div class="card-media"><img class="card-image" src="' + H5P.getPath(this.options.cardImage.path, this.id) + '"></div>');
-    }
-
-    if (this.options.cardTitle) {
-      cardTarget.append(`<div class="card-body"></div>`);
-      var cardTitle = `<h3 class="card-title">${this.options.cardTitle}</h3>`;
-      $(".card-body").append(cardTitle);
-    }
-
-    if (this.options.cardText) {
-      var cardText = `<div class="card-text">${this.options.cardText}</div>`;
-      $(".card-body").append(cardText);
-    }
-
-
-    //var sanitizedUrl = sanitizeUrlProtocol(url);
-
-
-    if (this.options.cardAction.url && this.options.cardAction.label) {
-
-      var url = '';
-      if (this.options.cardAction.protocol !== 'other') {
-        url += this.options.cardAction.protocol;
+      // Add image if provided.
+      if (this.options.cards[i].cardImage && this.options.cards[i].cardImage.path) {
+        var cardMedia = `<div class="card-media"><img class="card-image" src="${H5P.getPath(this.options.cards[i].cardImage.path, this.id)}"></div>`
+        cardTarget.append(cardMedia);
+        //$cardTarget.append('<div class="card-media"><img class="card-image" src="' + H5P.getPath(this.options.cardImage.path, this.id) + '"></div>');
       }
-      url += this.options.cardAction.url;
+
+      if (this.options.cards[i].cardTitle) {
+        cardTarget.append(`<div class="card-body"></div>`);
+        var cardTitle = `<h3 class="card-title">${this.options.cards[i].cardTitle}</h3>`;
+        $(".card-body").append(cardTitle);
+      }
+
+      if (this.options.cardText) {
+        var cardText = `<div class="card-text">${this.options.cards[i].cardText}</div>`;
+        $(".card-body").append(cardText);
+      }
 
 
-      var cardAction = `<div class="card-action"><a href="card-action" href="${url}" target="_blank">${this.options.cardAction.label}</a>`;
+      //var sanitizedUrl = sanitizeUrlProtocol(url);
 
-      cardTarget.append(cardAction);
-    }
 
-  };
- 
-  return C;
+      if (this.options.cards[i].cardAction.url && this.options.cards[i].cardAction.label) {
+
+        var url = '';
+        if (this.options.cards[i].cardAction.protocol !== 'other') {
+          url += this.options.cards[i].cardAction.protocol;
+        }
+        url += this.options.cards[i].cardAction.url;
+
+
+        var cardAction = `<div class="card-action"><a href="card-action" href="${url}" target="_blank">${this.options.cards[i].cardAction.label}</a>`;
+
+        cardTarget.append(cardAction);
+      }
+
+    };
+  
+    return C;
+  }
 })(H5P.jQuery);
