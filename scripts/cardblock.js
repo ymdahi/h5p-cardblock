6b05fa96cdbe3eb16fb34622a0c6a64bb836aa76
ydahi <ymdahi@gmail.com>
2019-11-21 10:09:15 -0500

    use better dom creation and searching methods

--- scripts/cardblock.js
+++ scripts/cardblock.js
@@ -35,9 +35,9 @@ H5P.CardBlock = (function ($) {
       if (currentCard.params.cardImage && currentCard.params.cardImage.path) {
         var cardMedia = `<div class="card-media"><img class="card-image" src="${H5P.getPath(currentCard.params.cardImage.path, this.id)}"></div>`;
         console.log(cardContainer);
-        $('.cardblock').append(cardMedia);
+        $('.cardblock .card-' + i).append(cardMedia);
       } else {
-        $('.cardblock').addClass('no-media');
+        $('.cardblock .card-' + i).addClass('no-media');
       }
 
       // // add card title if provided
